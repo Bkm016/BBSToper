9df7fae7a62e794cb12730efe233e1f5f1b40c39
坏黑 <b@skymc.me>
2023-06-30 15:58:35 +0800

    1

--- src/moe/feo/bbstoper/gui/GUI.java
+++ src/moe/feo/bbstoper/gui/GUI.java
@@ -3,6 +3,7 @@ package moe.feo.bbstoper.gui;
 import java.util.ArrayList;
 import java.util.List;
 
+import me.clip.placeholderapi.PlaceholderAPI;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.SkullType;
@@ -47,18 +48,20 @@ public class GUI {
 	@SuppressWarnings("deprecation")
 	public void createGui(Player player) {
 		InventoryHolder holder = new BBSToperGUIHolder();
-		this.setGui(Bukkit.createInventory(holder, InventoryType.CHEST, getTitle()));
-		for (int i = 0; i < inv.getSize(); i++) {// 设置边框
-			if (i > 9 && i < 17)
-				continue;
-			inv.setItem(i, getRandomPane());
-		}
-		ItemStack skull;
-		try {
-			skull = new ItemStack(Material.SKULL_ITEM, 1, (short) SkullType.PLAYER.ordinal());
-		} catch (NoSuchFieldError e) {// 某些高版本服务端不兼容旧版写法
-			skull = new ItemStack(Material.getMaterial("PLAYER_HEAD"), 1);
-		}
+		setGui(Bukkit.createInventory(holder, InventoryType.CHEST, getTitle()));
+//		for (int i = 0; i < inv.getSize(); i++) {// 设置边框
+//			if (i > 9 && i < 17) {
+//                continue;
+//            }
+//			inv.setItem(i, getRandomPane());
+//		}
+
+		ItemStack skull = new ItemStack(Material.PAPER);
+//		try {
+//			skull = new ItemStack(Material.SKULL_ITEM, 1, (short) SkullType.PLAYER.ordinal());
+//		} catch (NoSuchFieldError e) {// 某些高版本服务端不兼容旧版写法
+//			skull = new ItemStack(Material.getMaterial("PLAYER_HEAD"), 1);
+//		}
 		SkullMeta skullmeta = (SkullMeta) skull.getItemMeta();// 玩家头颅
 		if (Option.GUI_DISPLAYHEADSKIN.getBoolean()) {// 如果开启了头颅显示，才会设置头颅的所有者
 			try {
@@ -79,16 +82,20 @@ public class GUI {
 			skulllores.add(Message.GUI_CLICKREBOUND.getString());
 		}
 		skullmeta.setLore(skulllores);
+		skullmeta.setCustomModelData(95274);
 		skull.setItemMeta(skullmeta);
 		inv.setItem(12, skull);
-		ItemStack sunflower;
-		try {
-			sunflower = new ItemStack(Material.DOUBLE_PLANT);
-		} catch (NoSuchFieldError e) {// 某些高版本服务端不兼容旧版写法
-			sunflower = new ItemStack(Material.getMaterial("SUNFLOWER"));
-		}
-		ItemMeta sunflowermeta = sunflower.getItemMeta();
-		sunflowermeta.setDisplayName(Message.GUI_REWARDS.getString());
+//		ItemStack sunflower;
+//		try {
+//			sunflower = new ItemStack(Material.DOUBLE_PLANT);
+//		} catch (NoSuchFieldError e) {// 某些高版本服务端不兼容旧版写法
+//			sunflower = new ItemStack(Material.getMaterial("SUNFLOWER"));
+//		}
+
+		// 领取奖励
+		ItemStack rewardItem = new ItemStack(Material.PAPER);
+		ItemMeta rewardItemMeta = rewardItem.getItemMeta();
+		rewardItemMeta.setDisplayName(Message.GUI_REWARDS.getString());
 		List<String> sunflowerlores = new ArrayList<String>(Message.GUI_REWARDSINFO.getStringList());// 自定义奖励信息
 		if (sunflowerlores.isEmpty()) {// 如果没有自定义奖励信息
 			sunflowerlores.addAll(Option.REWARD_COMMANDS.getStringList());// 直接显示命令
@@ -102,32 +109,40 @@ public class GUI {
 			}
 		}
 		sunflowerlores.add(Message.GUI_CLICKGET.getString());
-		sunflowermeta.setLore(sunflowerlores);
-		sunflower.setItemMeta(sunflowermeta);
-		inv.setItem(13, sunflower);
-		ItemStack star = new ItemStack(Material.NETHER_STAR);
-		ItemMeta starmeta = star.getItemMeta();
-		starmeta.setDisplayName(Message.GUI_TOPS.getString());
+		sunflowerlores = PlaceholderAPI.setPlaceholders(player, sunflowerlores);// 替换占位符
+		rewardItemMeta.setLore(sunflowerlores);
+		rewardItemMeta.setCustomModelData(95271);
+		rewardItem.setItemMeta(rewardItemMeta);
+		inv.setItem(13, rewardItem);
+
+		// 排行榜
+		ItemStack top = new ItemStack(Material.PAPER);
+		ItemMeta topMeta = top.getItemMeta();
+		topMeta.setDisplayName(Message.GUI_TOPS.getString());
 		List<String> starlores = new ArrayList<String>();
 		List<Poster> listposter = sql.getTopPosters();
 		for (int i = 0; i < listposter.size(); i++) {
-			if (i >= Option.GUI_TOPPLAYERS.getInt())
-				break;
+			if (i >= Option.GUI_TOPPLAYERS.getInt()) {
+                break;
+            }
 			starlores.add(Message.POSTERPLAYER.getString() + ":" + listposter.get(i).getName() + " "
 					+ Message.POSTERID.getString() + ":" + listposter.get(i).getBbsname() + " "
 					+ Message.POSTERNUM.getString() + ":" + listposter.get(i).getCount());
 		}
-		starmeta.setLore(starlores);
-		star.setItemMeta(starmeta);
-		inv.setItem(14, star);
-		ItemStack compass = new ItemStack(Material.COMPASS);
-		ItemMeta compassmeta = compass.getItemMeta();
-		compassmeta.setDisplayName(Message.GUI_PAGESTATE.getString());
+		topMeta.setLore(starlores);
+		topMeta.setCustomModelData(95282);
+		top.setItemMeta(topMeta);
+		inv.setItem(14, top);
+
+		// 信息
+		ItemStack info = new ItemStack(Material.PAPER);
+		ItemMeta infoMeta = info.getItemMeta();
+		infoMeta.setDisplayName(Message.GUI_PAGESTATE.getString());
 		List<String> compasslores = new ArrayList<String>();
 		compasslores.add(Message.GUI_PAGEID.getString().replaceAll("%PAGEID%", Option.MCBBS_URL.getString()));
 		Crawler crawler = new Crawler();
 		if (crawler.visible) {// 如果帖子可视，就获取帖子最近一次顶贴
-			if (crawler.Time.size() > 0) { // 如果从没有人顶帖，就以“----”代替上次顶帖时间(原来不加判断直接get会报索引范围错误)
+			if (!crawler.Time.isEmpty()) { // 如果从没有人顶帖，就以“----”代替上次顶帖时间(原来不加判断直接get会报索引范围错误)
 				compasslores.add(Message.GUI_LASTPOST.getString().replaceAll("%TIME%", crawler.Time.get(0)));
 			} else {
 				compasslores.add(Message.GUI_LASTPOST.getString().replaceAll("%TIME%", "----"));
@@ -141,28 +156,24 @@ public class GUI {
 			compasslores.add(extrarewards);
 		}
 		compasslores.add(Message.GUI_CLICKOPEN.getString());
-		compassmeta.setLore(compasslores);
-		compass.setItemMeta(compassmeta);
-		inv.setItem(22, compass);
+		infoMeta.setLore(compasslores);
+		infoMeta.setCustomModelData(95280);
+		info.setItemMeta(infoMeta);
+		inv.setItem(22, info);
 	}
 	
 	public ItemStack getRandomPane() {// 获取随机一种颜色的玻璃板
-		short data = (short)(Math.random()* 16);// 这会随机取出0-15的数据值
-		while (data == 8) {// 8号亮灰色染色玻璃板根本没有颜色
-			data = (short)(Math.random()* 16);
-		}
-		ItemStack frame;
-		try {
-			frame = new ItemStack(Material.STAINED_GLASS_PANE, 1, data);
-
-		} catch (NoSuchFieldError e) {// 某些高版本服务端不兼容旧版写法
-			String[] glasspanes = {"WHITE_STAINED_GLASS_PANE", "ORANGE_STAINED_GLASS_PANE", "MAGENTA_STAINED_GLASS_PANE",
-			"LIGHT_BLUE_STAINED_GLASS_PANE", "YELLOW_STAINED_GLASS_PANE", "LIME_STAINED_GLASS_PANE", "PINK_STAINED_GLASS_PANE",
-			"GRAY_STAINED_GLASS_PANE", "LIGHT_GRAY_STAINED_GLASS_PANE", "CYAN_STAINED_GLASS_PANE", "PURPLE_STAINED_GLASS_PANE",
-			"BLUE_STAINED_GLASS_PANE", "BROWN_STAINED_GLASS_PANE", "GREEN_STAINED_GLASS_PANE", "RED_STAINED_GLASS_PANE",
-			"BLACK_STAINED_GLASS_PANE"};
-			frame = new ItemStack(Material.getMaterial(glasspanes[data]), 1);
-		}
+//		short data = (short)(Math.random()* 16);// 这会随机取出0-15的数据值
+//		while (data == 8) {// 8号亮灰色染色玻璃板根本没有颜色
+//			data = (short)(Math.random()* 16);
+//		}
+//		ItemStack frame;
+//		String[] glasspanes = {"WHITE_STAINED_GLASS_PANE", "ORANGE_STAINED_GLASS_PANE", "MAGENTA_STAINED_GLASS_PANE",
+//				"LIGHT_BLUE_STAINED_GLASS_PANE", "YELLOW_STAINED_GLASS_PANE", "LIME_STAINED_GLASS_PANE", "PINK_STAINED_GLASS_PANE",
+//				"GRAY_STAINED_GLASS_PANE", "LIGHT_GRAY_STAINED_GLASS_PANE", "CYAN_STAINED_GLASS_PANE", "PURPLE_STAINED_GLASS_PANE",
+//				"BLUE_STAINED_GLASS_PANE", "BROWN_STAINED_GLASS_PANE", "GREEN_STAINED_GLASS_PANE", "RED_STAINED_GLASS_PANE",
+//				"BLACK_STAINED_GLASS_PANE"};
+		ItemStack frame = new ItemStack(Material.BLACK_STAINED_GLASS_PANE);
 		ItemMeta framemeta = frame.getItemMeta();
 		framemeta.setDisplayName(Message.GUI_FRAME.getString());
 		frame.setItemMeta(framemeta);
